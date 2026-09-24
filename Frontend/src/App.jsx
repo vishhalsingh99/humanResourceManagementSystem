b4399421a5e69f5db79552a8d76ec74dc1fe16c3
Vishal Singh <vsr89541@gmail.com>
2026-09-24 17:49:47 +0530

    Fix Vercel router base path

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClientProvider } from '@tanstack/react-query';
 import { AppProvider } from './context/AppContext';
-import { ThemeProvider } from './context/ThemeContext';
-import { queryClient } from './lib/queryClient';
 import AppShell from './components/layout/AppShell';
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <BrowserRouter basename="/hrms/app">
-          <AppProvider>
-            <AppShell />
-          </AppProvider>
-        </BrowserRouter>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <AppShell />
+      </AppProvider>
+    </BrowserRouter>
   );
 }
